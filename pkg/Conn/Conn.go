9bf75f82ae893bd9956484104eef21350db30e9b
matoval <mat24c@gmail.com>
2024-08-30 19:02:24 -0700

    test connection

--- pkg/Conn/Conn.go
+++ pkg/Conn/Conn.go
@@ -31,14 +31,46 @@ func NewConn() {
 		if err != nil {
 			panic(fmt.Errorf("Fatal error creating hub listener"))
 		}
-		for {
-			conn, err := ln.Accept(ctx)
-			if err != nil {
-				panic(fmt.Errorf("Fatal error accepting connection"))
+		go func() {
+			for {
+				fmt.Println("Here")
+				conn, err := ln.Accept(ctx)
+				if err != nil {
+					panic(fmt.Errorf("Fatal error accepting connection"))
+				}
+				fmt.Println("hi")
+				fmt.Printf("Remote Address: %s", conn.RemoteAddr())
 			}
-			fmt.Printf("Remote Address: %s", conn.RemoteAddr())
+		}()
+	}
+	if true {
+		addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:6121")
+		if err != nil {
+			panic(fmt.Errorf("Fatal error resolving addr: %w", err))
 		}
-	} else {
-
+		c, err := net.ListenUDP("udp", addr)
+		if err != nil {
+			panic(fmt.Errorf("Fatal error creating hub lisetn connect: %w", err))
+		}
+		cfg := &quic.Config{
+			HandshakeIdleTimeout: 20 * time.Second,
+			MaxIdleTimeout: 90 * time.Second,
+			Allow0RTT: true,
+			DisablePathMTUDiscovery: false,
+		}
+		tr := quic.Transport {
+			Conn: c,
+		}
+		ctx := context.Background()
+		conn, err := tr.Dial(ctx, addr, &tls.Config{}, cfg)
+		if err != nil {
+			panic(fmt.Errorf("Fatal error quic dialing: %w", err))
+		}
+		fmt.Printf("Remote Address: %s", conn.RemoteAddr())
+		stream, err := conn.OpenStreamSync(ctx)
+		if err != nil {
+			panic(fmt.Errorf("Fatal error quic stream: %w", err))
+		}
+		stream.Write([]byte("hello"))
 	}
 }
